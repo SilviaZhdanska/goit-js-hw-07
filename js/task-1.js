73412ad661e550eed2ac4515789c333cc90f7ccf
Silvia Zhdanska <silviaalbert1992@gmail.com>
2024-06-03 14:58:17 +0300

    task-1 completed

--- js/task-1.js
+++ js/task-1.js
@@ -3,5 +3,5 @@ console.log("Number of categories:", categorieList.length);
 
 categorieList.forEach((element) => {
   console.log("Category:", element.firstElementChild.textContent);
-  console.log("Elements: ", element.lastChild.length);
+  console.log("Elements: ", element.lastElementChild.children.length);
 });
