1fe2b3cd62db651f2ebec65364e909e471798803
Silvia Zhdanska <silviaalbert1992@gmail.com>
2024-06-05 22:59:15 +0300

    update

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,9 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+button.addEventListener("click", function);
+function createBoxes(amount) {}
+function destroyBoxes() {
+  element.remove();
+}
