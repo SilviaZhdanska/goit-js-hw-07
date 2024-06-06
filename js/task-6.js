a2f3b0d8e40587f5049051eda8abbf0c7dce64e6
Silvia Zhdanska <silviaalbert1992@gmail.com>
2024-06-06 20:33:38 +0300

    task6

--- js/task-6.js
+++ js/task-6.js
@@ -1,11 +1,38 @@
+const input = document.querySelector("input");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const boxesContainer = document.getElementById("boxes");
+
+createButton.addEventListener("click", createBoxesHandler);
+destroyButton.addEventListener("click", destroyBoxesHandler);
+
+function createBoxesHandler() {
+  const amount = Number(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
+  }
+}
+
+function destroyBoxesHandler() {
+  boxesContainer.innerHTML = "";
+}
+
+function createBoxes(amount) {
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = size + "px";
+    box.style.height = size + "px";
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.margin = 20 + "px";
+    boxesContainer.appendChild(box);
+    size += 10;
+  }
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
-}
-
-button.addEventListener("click", function);
-function createBoxes(amount) {}
-function destroyBoxes() {
-  element.remove();
+    .padStart(6, "0")}`;
 }
